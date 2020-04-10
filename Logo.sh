24e01b4d0fb91d342c518d26fdc31530b20db78b
DavidxxxLuckyxxx <63423108+DavidxxxLuckyxxx@users.noreply.github.com>
2020-04-10 12:24:37 -0700

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -8,17 +8,17 @@ yellow='\e[1;33m'
 echo ""
 echo""
 banner() {
- echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██████  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██████  ██   ██ ██ ███████   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   █  ██    ██    \e[0m''\e[1;33m\e[0m\e[1;32m ██   █  ██   ██ ██ ██        \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ███████ ██   ██ ██    ██ ███\e[0m''\e[1;33m\e[0m\e[1;32m ██████  ███████ ██ ███████   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   ██  ██  ██     \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██      ██   \e[0m'
- echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██████    ████      \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██ ███████   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m  █ █   █ █     ██        ██████      \e[0m''\e[1;33m\e[0m\e[1;32m ██████     ███        ███       █████    \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m  █  █ █  █    █  █      █      █     \e[0m''\e[1;33m\e[0m\e[1;32m ██       █     █    █     █     █    █   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m  █   █   █   ████ █    █        █    \e[0m''\e[1;33m\e[0m\e[1;32m ██████  █       █  █       █    █     █  \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m  █   █   █  █      █    █    ██ █    \e[0m''\e[1;33m\e[0m\e[1;32m     ██   █     █    █     █     █    █   \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m  █       █  █      █     ██████ ███  \e[0m''\e[1;33m\e[0m\e[1;32m ██████     ███        ███       █████    \e[0m'
   echo""    
   echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m''\e[1;33m\e[0m\e[1;34m██████████\e[0m''\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m OTP BYPASS PHISHING TOOL \e[0m''\e[1;31m\e[0m\e[1;31m [v 1.0] \e[0m'                                       
   echo ""
-   echo -e $'\e[1;33m\e[0m\e[1;33m      [ \e[0m\e[1;32m Follow on Github :- https://github.com/Ignitetch \e[0m \e[1;32m\e[0m\e[1;33m ] \e[0m'
+   echo -e $'\e[1;33m\e[0m\e[1;33m      [ \e[0m\e[1;32m Follow on FACEBOOK :-https://www.facebook.com/lucky.marri.102  \e[0m \e[1;32m\e[0m\e[1;33m ] \e[0m'
    echo ""
-      echo -e $'\e[1;37m\e[0m\e[1;37m  -------------------------------------------------------------------\e[0m'
-      echo -e $'\e[1;37m\e[0m\e[1;37m  --------------------------- \e[0m'
+      echo -e $'\e[1;37m\e[0m\e[1;37m            ---ANONYMOUS---\e[0m'
+      echo -e $'\e[1;37m\e[0m\e[1;37m  ---DAVID-LUCKY-SIDDY-VEER-BOSS-DADA---\e[0m'
       }
       banner
