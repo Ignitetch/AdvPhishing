461fb99db19937a4023cb7f9674e9d4e46762732
SG_PRO <55870659+Ignitetch@users.noreply.github.com>
2020-10-09 02:20:48 -0400

    2.2

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$sender_name = 'shubham'; 
+$sender_mail = 'secnhack@gmail.com';
+$sender_pass = 'shubham@##@123';
+
+$receiver_name = 'shubham';
+$receiver_mail = 'secnhack@gmail.com';
+
+?>
